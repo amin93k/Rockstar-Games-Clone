48bd85c5131c3a960b292a09efb20159146b1b93
Amin <aminkusheshvatan@gmail.com>
2025-03-14 13:42:20 +0330

    fixed logo image and search responsive

--- src/components/navbar/Logo.tsx
+++ src/components/navbar/Logo.tsx
@@ -4,7 +4,7 @@ export default function Logo() {
     return (
         <HStack flexShrink="0">
             <Image
-                src="src/assets/RAGE_Technology_Group_Logo.svg.png"
+                src="/assets/RAGE_Technology_Group_Logo.png"
                 alt='logo'
                 boxSize='30px'
                 objectFit='cover'
